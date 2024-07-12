d9400b8eeafac38d3cb6eeb04d0fc4637c5a2c28
darksnakepy <darkhuntshowdown@gmail.com>
2024-07-12 09:08:10 +0200

    style

--- src/components/auth/SignUp/Form.tsx
+++ src/components/auth/SignUp/Form.tsx
@@ -26,7 +26,7 @@ const SignUp = () =>{
         <div className="max-w-sm w-full min-h-[350px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md flex justify-center items-center border-2 border-[#27272a]">
             <form className="flex flex-col justify-center p-7 rounded-lg mb-4 text-white">
                     <h1 className="flex justify-center text-[28px]">Register</h1>
-                    <Input type="email" label="Email" className="mt-6 w-full" size="sm" />
+                    <Input type="email" label="Email" className="mt-6 w-full" size="sm" onValueChange={(value) => { setFormState(f => ({ ...f, email: value})) }} />
                     <Input
                         label="Master Password"
                         size="sm"
@@ -41,6 +41,10 @@ const SignUp = () =>{
                         }
                         type={isVisible ? "text" : "password"}
                         className="mt-4"
+                        isRequired
+                        onValueChange={(value) => {
+                            setFormState(f => ({ ...f, masterPass: value }))
+                          }}
                     />
                     <Input
                         label="Repeat Master Password"
@@ -56,8 +60,13 @@ const SignUp = () =>{
                         }
                         type={isMPVisible ? "text" : "password"}
                         className="mt-4"
+                        onValueChange={(value) => {
+                            setFormState(f => ({ ...f, repeatPass: value }))
+                          }}
                     />
-                    <Input type="text" label="Phone number (2FA)" className="mt-4" size="sm"/>
+                    <Input type="text" label="Phone number (2FA)" className="mt-4" size="sm" onValueChange={(value) => {
+                            setFormState(f => ({ ...f, phoneNumber: value }))
+                          }}/>
                     <Button className="max-w-sm mt-4" color="primary" href="/signup" variant="flat">
                         Sign Up
                     </Button>
