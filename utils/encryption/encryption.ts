aca222ef69cceeb54cc75db774ec575fe4396bf5
Gabriele Lopez <gabriele.ope@gmail.com>
2024-07-22 17:12:36 +0200

    added functions to encrypt and decrypt with a key

--- utils/encryption/encryption.ts
+++ utils/encryption/encryption.ts
@@ -14,7 +14,7 @@ async function deriveKey(password: string, salt: string) {
 }
 
 // password is the master password entered by the user at sign up
-export async function encrypt(text: string, password: string) {
+export async function encryptWithPassword(text: string, password: string) {
     const salt = crypto.randomBytes(SALT_LENGTH);
     const key = await deriveKey(password, salt.toString('hex'));
     const iv = crypto.randomBytes(IV_LENGTH);
@@ -28,9 +28,28 @@ export async function encrypt(text: string, password: string) {
     };
 }
 
-export async function decrypt(encryptedData: { salt: string; iv: string; data: string }, password: string) {
+export async function decryptWithPassword(encryptedData: { salt: string; iv: string; data: string }, password: string) {
     const salt = Buffer.from(encryptedData.salt, 'hex');
     const key = await deriveKey(password, salt.toString('hex'));
+    const iv = Buffer.from(encryptedData.iv, 'hex');
+    const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+    let decrypted = decipher.update(encryptedData.data, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+export async function encryptWithKey(text: string, key: string) {
+    const iv = crypto.randomBytes(IV_LENGTH);
+    const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return {
+        iv: iv.toString('hex'),
+        data: encrypted
+    };
+}
+
+export async function decryptWithKey(encryptedData: { iv: string; data: string }, key: string) {
     const iv = Buffer.from(encryptedData.iv, 'hex');
     const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
     let decrypted = decipher.update(encryptedData.data, 'hex', 'utf8');
