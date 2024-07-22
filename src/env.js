f34cdff93beda2fb7dc9dd8bc329b3c99a33ce72
Gabriele Lopez <gabriele.ope@gmail.com>
2024-07-22 17:16:01 +0200

    added AES key to .env

--- src/env.js
+++ src/env.js
@@ -11,6 +11,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    AES_KEY: z.string().min(64),
   },
 
   /**
@@ -29,6 +30,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    AES_KEY: process.env.AES_KEY
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
