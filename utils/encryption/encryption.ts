dd5114178f49504ca29f47c85f40232380cfa9ae
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-12 13:21:53 +0200

    Convert the key to a buffer

--- utils/encryption/encryption.ts
+++ utils/encryption/encryption.ts
@@ -3,8 +3,9 @@ const ALGORITHM = 'aes-256-cbc';
 const IV_LENGTH = 16;
 
 export async function encryptWithKey(text: string, key: string) {
+    const buffKey = Buffer.from(key, 'hex');
     const iv = crypto.randomBytes(IV_LENGTH);
-    const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
+    const cipher = crypto.createCipheriv(ALGORITHM, buffKey, iv);
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return {
