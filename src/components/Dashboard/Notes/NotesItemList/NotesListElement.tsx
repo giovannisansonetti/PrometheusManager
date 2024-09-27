8772881cfa5f95f7b2d2431c8aa4cef012db0eca
Gabriele Lopez <gabriele.ope@gmail.com>
2024-09-27 20:01:12 +0200

    Noteslistelement fixes

--- src/components/Dashboard/Notes/NotesItemList/NotesListElement.tsx
+++ src/components/Dashboard/Notes/NotesItemList/NotesListElement.tsx
@@ -1,15 +1,8 @@
-import { NotesListProps } from "../interfaces/NotesList.models";
+import { type NotesListProps } from "../interfaces/NotesList.models";
 import Image from "next/image";
 import React from "react";
 
-const NotesListItem = ({
-  title,
-  image,
-  date,
-  onClick,
-  onDelete,
-  onModify,
-}: NotesListProps) => {
+const NotesListItem = ({ title, image, date, onClick }: NotesListProps) => {
   return (
     <div
       className="relative top-5 mb-4 ml-[12%] flex h-20 w-[80%] cursor-pointer flex-row rounded-md border-1 border-[#27272a] text-white"
